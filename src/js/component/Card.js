f5333d6b32c9c37da45c549e41dcd5581d49336f
Sai40k <151876367+Sai40k@users.noreply.github.com>
2024-06-17 04:07:55 +0000

    ahora si

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -9,7 +9,7 @@ export const Card = (props) => {
 
     return (
         <div key={props.uid} className="card  p-1" style={{ width: "18rem" }}>
-            <img src="" className="card-img-top" alt="..." />
+            <img src={props.img} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
